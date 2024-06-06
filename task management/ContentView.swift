c3425f42dc18b8f5e2a3bded778002436b607e92
이준석 <ryan0826@naver.com>
2024-06-06 14:45:21 +0900

    Initial Commit

---
+++ task management/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  task management
+//
+//  Created by snlcom on 6/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ task management/task_managementApp.swift
@@ -0,0 +1,17 @@
+//
+//  task_managementApp.swift
+//  task management
+//
+//  Created by snlcom on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct task_managementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
